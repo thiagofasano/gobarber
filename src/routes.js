2b1f9236f6502cbf581b52ffe68d2c26d5fff655
thiagofasano <thiagofasano@gmail.com>
2019-07-08 20:23:35 -0300

    api end

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import AuthMiddleware from './app/middlewares/auth';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users/', UserController.store);
 routes.post('/sessions/', SessionController.store);
@@ -12,5 +22,19 @@ routes.post('/sessions/', SessionController.store);
 routes.use(AuthMiddleware);
 
 routes.put('/users/', UserController.update);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.get('/provider', ProviderController.index);
+routes.get('/provider/:providerId/available', AvailableController.index);
 
 export default routes;
